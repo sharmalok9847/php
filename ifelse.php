dd5cdcbaf460e127722d8690eae0ce74eb3f869f
sharmalok9847 <sharmalok9847@gmail.com>
2019-01-30 15:34:04 +0530

    add ifelse file

---
+++ ifelse.php
@@ -0,0 +1,24 @@
+<?php
+
+$a=12;
+$b=4;
+$c=6;
+
+if($a>$b && $a>$c)
+{
+	echo "a is greater ";
+}
+elseif ($b>$a) {
+	echo "b is greater t";
+	
+}
+elseif( $c>$a && $c>$b)
+{
+	echo "b is greater than a";
+}
+
+
+
+
+
+?>
